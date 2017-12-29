3c4ba7963d88a31baf801d3ab216c375fdf16753
Sofiane Gargouri <contact@sofianeg.com>
2017-12-30 00:20:00 +0100

    Updating PG extensions for Travis

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 20171229211311) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "uuid-ossp"
   enable_extension "pgcrypto"
 
   create_table "stores", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
