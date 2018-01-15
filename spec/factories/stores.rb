5c048579b2d701ea1648edc399b81aa3eabcfb4f
Montois Clement <montois.clem@gmail.com>
2018-01-15 21:46:57 +0100

    Modifying Factory Key (stores)

--- spec/factories/stores.rb
+++ spec/factories/stores.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :store do
-    key {Faker::Internet.domain_word}
-    name {Faker::Company.name}
+    name {Faker::StarWars.planet}
+    key {name.to_slug}
     address {Faker::Address.street_address}
     country {ISO3166::Country.all.map(&:alpha2).sample}
   end
