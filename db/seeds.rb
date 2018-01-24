90e1cf8d4bc531b3590f533d960382170b2cb895
Montois Clement <montois.clem@gmail.com>
2018-01-24 09:48:31 +0100

    Adding seeds for category + locale user

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@
 puts "Creating users"
 
 owner_user = User.create(email: 'owner@xanyah.io', firstname: 'Owner', lastname: 'User', confirmed_at: Time.now, password: '12345678')
-admin_user = User.create(email: 'admin@xanyah.io', firstname: 'Admin', lastname: 'User', confirmed_at: Time.now, password: '12345678')
+admin_user = User.create(email: 'admin@xanyah.io', firstname: 'Admin', lastname: 'User', confirmed_at: Time.now, password: '12345678', locale: 'fr')
 regular_user = User.create(email: 'regular@xanyah.io', firstname: 'Regular', lastname: 'User', confirmed_at: Time.now, password: '12345678')
 
 puts "Creating store"
@@ -18,8 +18,14 @@ StoreMembership.create(store: demo_store, user: owner_user, role: :owner)
 StoreMembership.create(store: demo_store, user: admin_user, role: :admin)
 StoreMembership.create(store: demo_store, user: regular_user, role: :regular)
 
-puts "Creating manufacturers, providers"
+puts "Creating manufacturers, providers, categories"
 5.times do
   Manufacturer.create(name: Faker::HarryPotter.character, store: demo_store)
   Provider.create(name: Faker::RickAndMorty.character, store: demo_store)
 end
+
+category = Category.create(label: Faker::Space.planet, store: demo_store, tva: :standard_rate)
+Category.create(label: Faker::Space.planet, store: demo_store, tva: :reduced_rate, category: category)
+Category.create(label: Faker::Space.planet, store: demo_store, tva: :reduced_rate_alt)
+Category.create(label: Faker::Space.planet, store: demo_store, tva: :super_reduced_rate)
+Category.create(label: Faker::Space.planet, store: demo_store, tva: :parking_rate, category: category)
