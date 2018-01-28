5a64ea5a527731ee95fe35be4abe698965602925
Clément <clement.montois@ig2i.fr>
2018-01-28 16:30:09 +0100

    Category seeds (#21)
    * Generate Complete API Doc
    
    * Adding seeds for category + locale user
    
    * Fixing categories creation in seeds
    
    * Updating user locale on seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,9 +3,9 @@
 
 puts "Creating users"
 
-owner_user = User.create(email: 'owner@xanyah.io', firstname: 'Owner', lastname: 'User', confirmed_at: Time.now, password: '12345678')
-admin_user = User.create(email: 'admin@xanyah.io', firstname: 'Admin', lastname: 'User', confirmed_at: Time.now, password: '12345678')
-regular_user = User.create(email: 'regular@xanyah.io', firstname: 'Regular', lastname: 'User', confirmed_at: Time.now, password: '12345678')
+owner_user = User.create(email: 'owner@xanyah.io', firstname: 'Owner', lastname: 'User', confirmed_at: Time.now, password: '12345678', locale: 'en')
+admin_user = User.create(email: 'admin@xanyah.io', firstname: 'Admin', lastname: 'User', confirmed_at: Time.now, password: '12345678', locale: 'fr')
+regular_user = User.create(email: 'regular@xanyah.io', firstname: 'Regular', lastname: 'User', confirmed_at: Time.now, password: '12345678', locale: 'en')
 
 puts "Creating store"
 
@@ -18,8 +18,21 @@ StoreMembership.create(store: demo_store, user: owner_user, role: :owner)
 StoreMembership.create(store: demo_store, user: admin_user, role: :admin)
 StoreMembership.create(store: demo_store, user: regular_user, role: :regular)
 
-puts "Creating manufacturers, providers"
+puts "Creating manufacturers, providers, categories"
 5.times do
   Manufacturer.create(name: Faker::HarryPotter.character, store: demo_store)
   Provider.create(name: Faker::RickAndMorty.character, store: demo_store)
+  Category.create(
+    label: Faker::Space.planet,
+    store: demo_store,
+    tva: %w(standard_rate reduced_rate reduced_rate_alt super_reduced_rate parking_rate).sample,
+    category: [
+      Category.create(
+        label: Faker::Space.planet,
+        store: demo_store,
+        tva: %w(standard_rate reduced_rate reduced_rate_alt super_reduced_rate parking_rate).sample
+      ),
+      nil
+    ].sample
+  )
 end
