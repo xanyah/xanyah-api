8ffa04b90d5fcb7de4d5667a2475a73fe7c405ad
Sofiane Gargouri <contact@sofianeg.com>
2018-01-30 10:52:16 +0100

    Refactoring

--- spec/routing/custom_attributes_routing_spec.rb
+++ spec/routing/custom_attributes_routing_spec.rb
@@ -4,29 +4,29 @@ RSpec.describe CustomAttributesController, type: :routing do
   describe "routing" do
 
     it "routes to #index" do
-      expect(:get => "/custom_attributes").to route_to("custom_attributes#index")
+      expect(get: "/custom_attributes").to route_to("custom_attributes#index")
     end
 
 
     it "routes to #show" do
-      expect(:get => "/custom_attributes/1").to route_to("custom_attributes#show", :id => "1")
+      expect(get: "/custom_attributes/1").to route_to("custom_attributes#show", id: "1")
     end
 
 
     it "routes to #create" do
-      expect(:post => "/custom_attributes").to route_to("custom_attributes#create")
+      expect(post: "/custom_attributes").to route_to("custom_attributes#create")
     end
 
     it "routes to #update via PUT" do
-      expect(:put => "/custom_attributes/1").to route_to("custom_attributes#update", :id => "1")
+      expect(put: "/custom_attributes/1").to route_to("custom_attributes#update", id: "1")
     end
 
     it "routes to #update via PATCH" do
-      expect(:patch => "/custom_attributes/1").to route_to("custom_attributes#update", :id => "1")
+      expect(patch: "/custom_attributes/1").to route_to("custom_attributes#update", id: "1")
     end
 
     it "routes to #destroy" do
-      expect(:delete => "/custom_attributes/1").to route_to("custom_attributes#destroy", :id => "1")
+      expect(delete: "/custom_attributes/1").to route_to("custom_attributes#destroy", id: "1")
     end
 
   end
--- spec/routing/variant_attributes_routing_spec.rb
+++ spec/routing/variant_attributes_routing_spec.rb
@@ -4,29 +4,29 @@ RSpec.describe VariantAttributesController, type: :routing do
   describe "routing" do
 
     it "routes to #index" do
-      expect(:get => "/variant_attributes").to route_to("variant_attributes#index")
+      expect(get: "/variant_attributes").to route_to("variant_attributes#index")
     end
 
 
     it "routes to #show" do
-      expect(:get => "/variant_attributes/1").to route_to("variant_attributes#show", :id => "1")
+      expect(get: "/variant_attributes/1").to route_to("variant_attributes#show", id: "1")
     end
 
 
     it "routes to #create" do
-      expect(:post => "/variant_attributes").to route_to("variant_attributes#create")
+      expect(post: "/variant_attributes").to route_to("variant_attributes#create")
     end
 
     it "routes to #update via PUT" do
-      expect(:put => "/variant_attributes/1").to route_to("variant_attributes#update", :id => "1")
+      expect(put: "/variant_attributes/1").to route_to("variant_attributes#update", id: "1")
     end
 
     it "routes to #update via PATCH" do
-      expect(:patch => "/variant_attributes/1").to route_to("variant_attributes#update", :id => "1")
+      expect(patch: "/variant_attributes/1").to route_to("variant_attributes#update", id: "1")
     end
 
     it "routes to #destroy" do
-      expect(:delete => "/variant_attributes/1").to route_to("variant_attributes#destroy", :id => "1")
+      expect(:delete => "/variant_attributes/1").to route_to("variant_attributes#destroy", id: "1")
     end
 
   end
