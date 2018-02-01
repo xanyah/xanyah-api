155308907079a4e91dfa6384d68eb4484fd0e3e5
Sofiane Gargouri <contact@sofianeg.com>
2018-02-01 16:10:02 +0100

    Fixing seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 
-logger.info "Creating users"
+puts "Creating users"
 
 owner_user = User.create(
   email: "owner@xanyah.io",
