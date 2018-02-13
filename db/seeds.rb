8b731940e5027c687298f7c54580100e83f2b244
Sofiane Gargouri <contact@sofianeg.com>
2018-02-13 15:27:25 +0100

    Updating seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-
 puts "Creating users"
 
-owner_user = User.create(
+owner_user = User.create!(
   email: "owner@xanyah.io",
   firstname: "Owner", lastname: "User",
   confirmed_at: Time.zone.now,
   password: "12345678",
   locale: "en"
 )
-admin_user = User.create(
+admin_user = User.create!(
   email:        "admin@xanyah.io",
   firstname:    "Admin",
   lastname:     "User",
@@ -18,7 +15,7 @@ admin_user = User.create(
   password:     "12345678",
   locale:       "fr"
 )
-regular_user = User.create(
+regular_user = User.create!(
   email:        "regular@xanyah.io",
   firstname:    "Regular",
   lastname:     "User",
@@ -30,30 +27,48 @@ regular_user = User.create(
 puts "Creating store"
 
 store_name = Faker::StarWars.planet
-demo_store = Store.create(name: store_name, country: "FR", key: store_name.to_slug)
+demo_store = Store.create!(name: store_name, country: "FR", key: store_name.to_slug)
 
 puts "Creating store membership"
 
-StoreMembership.create(store: demo_store, user: owner_user, role: :owner)
-StoreMembership.create(store: demo_store, user: admin_user, role: :admin)
-StoreMembership.create(store: demo_store, user: regular_user, role: :regular)
+StoreMembership.create!(store: demo_store, user: owner_user, role: :owner)
+StoreMembership.create!(store: demo_store, user: admin_user, role: :admin)
+StoreMembership.create!(store: demo_store, user: regular_user, role: :regular)
 
 puts "Creating manufacturers, providers, categories"
 
 5.times do
-  Manufacturer.create(name: Faker::HarryPotter.character, store: demo_store)
-  Provider.create(name: Faker::RickAndMorty.character, store: demo_store)
-  Category.create(
+  Manufacturer.create!(name: Faker::HarryPotter.character, store: demo_store)
+  Provider.create!(name: Faker::RickAndMorty.character, store: demo_store)
+  Category.create!(
     name:     Faker::Space.planet,
     store:    demo_store,
     tva:      %w[standard_rate reduced_rate reduced_rate_alt super_reduced_rate parking_rate].sample,
     category: [
-      Category.create(
+      Category.create!(
         name:  Faker::Space.planet,
         store: demo_store,
         tva:   %w[standard_rate reduced_rate reduced_rate_alt super_reduced_rate parking_rate].sample
       ),
       nil
     ].sample
+  )
+end
+
+100.times do |idx|
+  price = rand(5...100)
+  Variant.create!(
+    product: Product.create!(
+      name: "#{Faker::Food.dish} ##{idx}",
+      store: demo_store,
+      manufacturer: Manufacturer.all.sample,
+      category: Category.all.sample,
+      ),
+      provider: Provider.all.sample,
+    original_barcode: idx.to_s.rjust(10, "0"),
+    buying_price: price,
+    tax_free_price: price * 1.5,
+    ratio: 1.5,
+    quantity: rand(0...20),
   )
 end
