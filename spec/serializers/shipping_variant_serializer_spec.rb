0800527f0597de6823b50b95c09c05a0b0d7affb
Sofiane Gargouri <contact@sofianeg.com>
2018-03-20 09:25:37 +0100

    Adding shipping variant serializer

---
+++ spec/serializers/shipping_variant_serializer_spec.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe ShippingVariantSerializer do
+  let(:shipping_variant) { create(:shipping_variant) }
+  let(:serializer) { described_class.new(shipping_variant) }
+  let(:serialization) { ActiveModelSerializers::Adapter.create(serializer) }
+  let(:json) { JSON.parse(serialization.to_json) }
+  let(:default_attributes) {
+    %w[
+      id
+      quantity
+      created_at
+      updated_at
+    ]
+  }
+
+  it :default_attributes do
+    default_attributes.each do |attribute|
+      if shipping_variant.send(attribute).class == ActiveSupport::TimeWithZone
+        expect(Time.zone.parse(json[attribute]).to_s).to eq(shipping_variant.send(attribute).to_s)
+      else
+        expect(json[attribute]).to eq(shipping_variant.send(attribute))
+      end
+    end
+  end
+
+  describe 'belongs_to' do
+    it :variant do
+      expect(json['variant']['id']).to eq(shipping_variant.variant.id)
+      expect(json['variant']['product']['id']).to eq(shipping_variant.variant.product.id)
+    end
+
+    it :provider do
+      expect(json['shipping']['id']).to eq(shipping_variant.shipping.id)
+    end
+  end
+end
