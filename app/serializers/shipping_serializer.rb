84f0211f985520251ae0ac6059c7a52e5d3a3e7e
Sofiane Gargouri <contact@sofianeg.com>
2018-03-27 14:03:39 +0200

    Updating shipping and inventory serializers

--- app/serializers/shipping_serializer.rb
+++ app/serializers/shipping_serializer.rb
@@ -2,9 +2,14 @@
 
 class ShippingSerializer < ActiveModel::Serializer
   attributes :id,
+             :shipping_variants_count,
              :locked_at,
              :created_at,
              :updated_at
 
   belongs_to :provider
+
+  def shipping_variants_count
+    object.shipping_variants.size
+  end
 end
