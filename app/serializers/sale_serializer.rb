4e24a8e3bd0450551fc51456f44275183c74878d
Clément <clement.montois@ig2i.fr>
2018-05-13 13:31:11 +0200

    sale serializer (#64)

--- app/serializers/sale_serializer.rb
+++ app/serializers/sale_serializer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class SaleSerializer < ActiveModel::Serializer
-  attributes :id, :total_price
+  attributes :id, :total_price, :created_at
 
   belongs_to :client
   belongs_to :store
