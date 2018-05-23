fdff62cb1bbbe7937ad7c847870d067af16489df
Sofiane Gargouri <contact@sofianeg.com>
2018-05-23 13:28:18 +0200

    Fixing order serializer

--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class OrderSerializer < ActiveModel::Serializer
-  attributes :id, :status
-  has_one :client
-  has_one :store
+  attributes :id, :status, :created_at
+  belongs_to :client
+  has_many :order_variants
 end
--- app/serializers/order_variant_serializer.rb
+++ app/serializers/order_variant_serializer.rb
@@ -3,5 +3,4 @@
 class OrderVariantSerializer < ActiveModel::Serializer
   attributes :id, :quantity
   has_one :variant
-  has_one :order
 end
