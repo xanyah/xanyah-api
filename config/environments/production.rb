d37f8fcbc8199dd44fee31155a155170c247b6a7
Sofiane Gargouri <contact@sofianeg.com>
2018-05-23 13:42:15 +0200

    Fixing email sending

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,6 +59,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
     address:              ENV['SMTP_ADDRESS'],
+    authentication:       'plain',
     enable_starttls_auto: true,
     user_name:            ENV['SMTP_USERNAME'],
     password:             ENV['SMTP_PASSWORD'],
