4872e26ce8ea8df1dfe738bd19544bdacedf2c61
Sofiane Gargouri <contact@sofianeg.com>
2018-09-17 15:06:28 +0200

    Fixing Heroku config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,15 @@
 # frozen_string_literal: true
 
 Sidekiq.configure_client do |config|
-  config.redis = {namespace: 'Xanyah'}
+  config.redis = {
+    namespace: 'Xanyah',
+    url:       ENV['REDIS_URL']
+  }
 end
 
 Sidekiq.configure_server do |config|
-  config.redis = {namespace: 'Xanyah'}
+  config.redis = {
+    namespace: 'Xanyah',
+    url:       ENV['REDIS_URL']
+  }
 end
