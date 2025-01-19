50b04e893fc525525c311d25b4eb61dc5afb60a0
Sofiane <contact@sofianeg.com>
2025-01-19 12:48:46 +0000

    Updating RSpec API Documentation

--- spec/acceptance_helper.rb
+++ spec/acceptance_helper.rb
@@ -6,7 +6,7 @@ require 'rspec_api_documentation/dsl'
 
 RspecApiDocumentation.configure do |config|
   # config.client_method = :client
-  config.docs_dir = Rails.root.join('docs')
+  config.docs_dir = Rails.root.join('docs/api')
   config.format = :open_api
   config.request_body_formatter = :json
   config.request_headers_to_include = %w[Content-Type Accept Access-Token Token-Type Client Expiry Uid]
