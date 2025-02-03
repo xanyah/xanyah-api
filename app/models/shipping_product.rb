0e52e6341c5ae58415fae46016219375984ecb5b
Sofiane <contact@sofianeg.com>
2025-02-03 20:11:29 +0000

    Fixing shipping product creation

--- app/models/shipping_product.rb
+++ app/models/shipping_product.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 class ShippingProduct < ApplicationRecord
-  belongs_to :shipping, optional: false
   belongs_to :product, optional: false
+  belongs_to :shipping, optional: false
 
-  has_one :store, through: :shipping
+  has_one :store, through: :product
 
-  validates_ownership_of :product, with: :store
+  validates_ownership_of :shipping, with: :store
 
   validates :product_id, uniqueness: { scope: :shipping_id }
   validate :shipping_locked
